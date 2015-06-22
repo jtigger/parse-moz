146ad480c3df83ec6dde74f0ee0b83773f5b4fc2
John S. Ryan <jtigger@infosysengr.com>
2015-06-21 23:13:15 -0700

    Beginning to capture individual MoZs
    - still need to identify correct quote.
    - can we pre-scrub the text to avoid encoding problems?

--- moz-from-txt2json.py
+++ moz-from-txt2json.py
@@ -15,21 +15,30 @@ for m in re.finditer(r"(Monday|Tuesday|Wednesday|Thursday|Friday)", raw_moz_data
   raw_mozs.append(raw_moz_data[index_of_start_of_last_moz:m.start()])
   index_of_start_of_last_moz = m.start()
 
-for moz in raw_mozs:
-  images = re.finditer(r"!\[([^\]]+)\]\(([^\)]+)\)", moz)
-  links = re.finditer(r"[^!]\[([^\]]+)\]\(([^\)]+)\)", moz)
-  quotes = re.finditer(r'["“]+(.*)["”]+', moz)
-  explores = re.finditer(r"(explore|pro-tip|pro tip):\**(.*)", moz, flags=re.IGNORECASE)
+mozs = []
+for raw_moz in raw_mozs:
+  images = re.finditer(r"!\[([^\]]+)\]\(([^\)]+)\)", raw_moz)
+  links = re.finditer(r"[^!]\[([^\]]+)\]\(([^\)]+)\)", raw_moz)
+  quotes = re.finditer(r'["“]+(.*)["”]+', raw_moz)
+  explores = re.finditer(r"(explore|pro-tip|pro tip):\**(.*)", raw_moz, flags=re.IGNORECASE)
   print "===- start -==="
-  print moz
+  print raw_moz
   print "--- parse ---"
+  moz = {}
   for image in images:
     print "image: { url: " + image.group(2) + "; alt-text: " + image.group(1) + " }"
   for link in links:
     print "link: { url: " + link.group(2) + "; text: " + link.group(1) + " }"
   for quote in quotes:
+    moz["quote"] = quote.group(0).replace("“","").replace("”","")
     print "quote: " + quote.group(0)
   for explore in explores:
     print explore.group(1) + ": " + explore.group(2)
   print "===- end -==="
+  if "quote" in moz and moz["quote"]:
+    print "Appending moz = " +moz["quote"]
+    mozs.append(moz)
 
+moz = { "mozs" : mozs }
+print moz
+print json.dumps(moz)
